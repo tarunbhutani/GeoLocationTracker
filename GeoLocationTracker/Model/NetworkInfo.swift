e2f24e95bd125f8aec6ccf0917b757a3eb8632b8
tarunbhutani3 <tarunbhutani652@gmail.com>
2019-02-15 16:50:23 +0800

    create dummy ui template for map and add geofence area

---
+++ GeoLocationTracker/Model/NetworkInfo.swift
@@ -0,0 +1,51 @@
+//
+//  NetworkInfo.swift
+//  GeoLocationTracker
+//
+//  Created by Tarun Bhutani on 15/02/2019.
+//  Copyright © 2019 Tarun Bhutani. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration.CaptiveNetwork
+
+struct NetworkInfo : CustomStringConvertible {
+    
+    
+    public let interface:String
+    public let ssid:String
+    public let bssid:String
+    init(_ interface:String, _ ssid:String,_ bssid:String) {
+        self.interface = interface
+        self.ssid = ssid
+        self.bssid = bssid
+    }
+    var description: String{
+        return "interface: " + interface + " ssid: " + ssid + " bssid: " + bssid
+    }
+    
+}
+
+struct NetworkList {
+    static func getNetworkInfos() -> Array<NetworkInfo> {
+        // https://forums.developer.apple.com/thread/50302
+        guard let interfaceNames = CNCopySupportedInterfaces() as? [String] else {
+            return []
+        }
+        let networkInfos:[NetworkInfo] = interfaceNames.compactMap{ name in
+            guard let info = CNCopyCurrentNetworkInfo(name as CFString) as? [String:AnyObject] else {
+                return nil
+            }
+            guard let ssid = info[kCNNetworkInfoKeySSID as String] as? String else {
+                return nil
+            }
+            guard let bssid = info[kCNNetworkInfoKeyBSSID as String] as? String else {
+                return nil
+            }
+            return NetworkInfo(name, ssid,bssid)
+        }
+        return networkInfos
+    }
+}
+
+
---
+++ GeoLocationTracker/ViewController/TabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarViewController.swift
+//  GeoLocationTracker
+//
+//  Created by InSynchro M SDN BHD on 15/02/2019.
+//  Copyright © 2019 Tarun Bhutani. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
